6bb773e83da151410ca971863aa1f7132abd9161
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2018-09-13 16:29:29 +0000

    Input, shows the date today

--- routes/mindays.js
+++ routes/mindays.js
@@ -4,7 +4,24 @@ var express = require("express"),
 	
 /* INDEX */
 router.get("/", function(req, res){
-	res.render("index");
+	var getToday = new Date();
+	var day = getToday.getDate();
+	var months = new Array();
+		months[0]  = "January";
+		months[1]  = "February";
+		months[2]  = "March";
+		months[3]  = "April";
+		months[4]  = "May";
+		months[5]  = "June";
+		months[6]  = "July";
+		months[7]  = "August";
+		months[8]  = "September";
+		months[9]  = "October";
+		months[10] = "November";
+		months[11] = "December";
+	var month = months[getToday.getMonth()]; //January is 0!
+	var date = day + " " + month;
+	res.render("index", {today: date});
 });
 
 router.post("/", function(req, res){
